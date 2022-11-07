f6e7588c2b19a26a4e7c2998922659aed2674dd5
fabiancsenge <csengefabian@gmail.com>
2022-11-07 14:59:49 +0100

    checking input file extension

--- backend/src/parser/main.cpp
+++ backend/src/parser/main.cpp
@@ -3,15 +3,18 @@
 
 int main(int argc, const char** argv) {
     if (argc < 2) {
-        std::cout << "Input file not given" << std::endl;;
+        std::cout << "Input file not given" << std::endl;
         return -1;
     }
     if (argc < 3) {
-        std::cout << "Output file name not given" << std::endl;;
+        std::cout << "Output file name not given" << std::endl;
         return -1;
     }
     std::string input_file = argv[1];
-    // TODO check format (.caff)
+    if(input_file.substr(input_file.find_last_of(".") + 1) != "caff") {
+        std::cout << "Input file must have .caff extension" << std::endl;
+        return -1;
+    }
     std::string output_file = argv[2];
 
     try {
@@ -19,7 +22,7 @@ int main(int argc, const char** argv) {
         converter.convert();
     }
     catch (const std::exception& e) {
-        std::cout << "Conversion faild with error: " << e.what() << std::endl;;
+        std::cout << "Conversion faild with error: " << e.what() << std::endl;
         return -1;
     }
     return 0;
